1cf6ef7d741cde4d7b39cf371d92c1714f29ac24
Charlotte Prevost <chaprevost91@gmail.com>
2018-09-21 09:48:10 -0500

    Commit 1

---
+++ javascript/app.js
@@ -0,0 +1,37 @@
+console.log("JS IS RUNNING");
+
+
+// Combine objects, arrays, and functions
+// COMPLETE ANY 3
+
+
+// Create an object that has a property that is an array. Log one of the elements of that array.
+const dogs = {
+	myDogs: ['Zelda', 'Jaena']
+}
+console.log(dogs.myDogs[0]);
+
+
+// Create an object that has a property that is a function (method). Call that method.
+const cats = {
+	meow(){
+		console.log("meooowww");
+	}
+}
+cats.meow();
+
+
+// Create an array that has a function as one of its elements. Call that function.
+const birds = [ 
+	chirp = () => {
+		console.log("chirpCHIRP I am BIRB");
+	}
+]
+birds[0]();
+
+// Commit 1 
+
+
+
+
+
