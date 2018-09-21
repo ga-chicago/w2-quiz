f589a74f2cf8a6b463ff45c6b3d2f7ccb1dd1592
Charlotte Prevost <chaprevost91@gmail.com>
2018-09-21 09:57:21 -0500

    Commit 2 - Combine objects, arrays, and functions more than one level deep

--- javascript/app.js
+++ javascript/app.js
@@ -34,4 +34,68 @@ birds[0]();
 
 
 
+// Combine objects, arrays, and functions more than one level deep
+// COMPLETE ANY 3
+
+
+// Create a function that returns an array. Log an element of the array.
+const snakes = () => {
+	return ['hisss', 'chomp!'];
+}
+console.log(snakes()[1]);
+
+// Create a function that returns an object that has an array. Log one of the elements of that array.
+const bears = () => {
+	return {
+		name: 'balou',
+		quotes: ['Petit d\'hom!', 'Il en faut peu pour etre heureux']
+	}
+}
+console.log(bears().quotes[1]);
+
+// Create an object that has a method that returns an object that has an object. Log a property of the inner object.
+const tigers = {
+	name: 'Sher Khan',
+	attack(){
+		return {
+			weapon: {claws: 'sharp', teeth: 'suPER SHARP'}
+		}
+	}
+}
+console.log(tigers.attack().weapon.teeth);
+
+
+// 🔴 The commit message should read: 
+// "Commit 2 - Combine objects, arrays, and functions more than one level deep"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
