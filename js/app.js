aebac4d2565db24f7b1cac7f8dc811a6f5af58a6
Gregory Frye <Swayze@Gregorys-MacBook-Pro.local>
2018-09-21 09:51:13 -0500

    Commit 1 - Combine objects, arrays, and functions

---
+++ js/app.js
@@ -0,0 +1,36 @@
+
+
+// Create an array that has an object in it.
+//  Log one of the properties of that object.
+const object0 = [
+{
+	name:'patrick'
+}
+
+];
+console.log(object0[0]);
+
+// Create an object that has a property that is an object. 
+// Log one of the properties of that inner object
+
+const object = {
+
+}
+
+{
+	name: 'Greg'
+}
+
+
+
+console.log(object.name);
+// Create an array that has an array as one of its elements. 
+// Log one of the elements of the inner array.
+
+const object2 = [0,1,[3,4]];
+
+console.log(object2[2]);
+
+// Create an array that has a function 
+// as one of its elements. Call that function.
+
