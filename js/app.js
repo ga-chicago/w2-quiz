41f18c7924c42c811a8a3dffaa982c0b22c51e85
Filip Tatarnik <ftatarnik@gmail.com>
2018-09-21 09:43:10 -0500

    one question done

---
+++ js/app.js
@@ -0,0 +1,26 @@
+console.log("works");
+
+// Create an object that has a property that is an array. Log one of the elements of that array.
+const fil = {
+	name: ["fil"],
+	hair: ["brown"]
+};
+console.log(fil.name[0]);
+console.log(fil.hair[0]);
+// Create an object that has a property that is an object. Log one of the properties of that inner object.
+const josh ={
+	name: {
+		"josh"
+	};
+};
+console.log(josh.name[1]);
+// Create an object that has a property that is a function (method). Call that method.
+// const funct1 ={
+	
+// }
+// Create an array that has an object in it. Log one of the properties of that object.
+// Create an array that has an array as one of its elements. Log one of the elements of the inner array.
+// Create an array that has a function as one of its elements. Call that function.
+// Create an object that has a property that is an array. Loop over the array and log each individual element.
+// Create an array that has an array as one of its elements. Loop over the second array and log each individual element.
+// Bonus: make each element of the outer array an array as well. Using two for loops, loop over each array in the outer array and log those elements.
